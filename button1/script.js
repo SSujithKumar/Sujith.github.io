d1552b64f94cb6ac34cff05a069a3fbec35f3ca2
SSujithKumar <140305534+SSujithKumar@users.noreply.github.com>
2023-08-01 18:13:40 +0530

    Add files via upload

---
+++ button1/script.js
@@ -0,0 +1,7 @@
+let btnSignin = document.querySelector('#sin');
+let btnSignup= document.querySelector('#sout');
+let btnCreate = document.querySelector('#cre');
+
+btnSignin.addEventListener('click',()=>btnSignin.style.backgroundColor='#337ab7')
+btnSignup.addEventListener('click',()=>btnSignup.style.backgroundColor='#c9302c')
+btnCreate.addEventListener('click',()=>btnCreate.style.backgroundColor='#4cae4c')
